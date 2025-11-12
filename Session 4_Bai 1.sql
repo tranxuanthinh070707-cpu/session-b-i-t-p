68e546b9e09448edbf98395bf3325839acf87bfb
tranxuanthinh070707-cpu <tranxuanthinh070707@gmail.com>
2025-11-12 23:48:17 +0700

    Add files via upload

---
+++ Session 4_Bai 1.sql
@@ -0,0 +1,13 @@
+
+CREATE DATABASE CompanyDB;
+
+USE companydb;
+
+CREATE TABLE Employees (
+	EmployeeID INT PRIMARY KEY AUTO_INCREMENT,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50)
+);
+
+
+SELECT * FROM `employees`;
